03b5c170bd3c4497c8c2aa967533a26d68249d78
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-25 04:09:33 -0500

    Update pedidos_detalle.routes.js

--- src/routes/pedidos_detalle.routes.js
+++ src/routes/pedidos_detalle.routes.js
@@ -6,7 +6,7 @@ const router=Router()
 
 router.get('/pedidos_detalle',getPedidos_Detalle)  //select
 router.get('/pedidos_detalle/:id',getpedidos_detallexid)  //select x id
-router.get('/pedidos/resumen', verificarToken, getPedidosResumen);
+router.get('/pedidos_detalle/resumen', verificarToken, getPedidosResumen);
 router.post('/pedidos_detalle',postPedido_Detalle)  //insert
 router.put('/pedidos_detalle/:id',putPedido_Detalle)  //update
 router.patch('/pedidos_detalle/:id',patchPedido_Detalle)  //update
