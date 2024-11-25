d6dfec0f1b6874d20b72ca8b2b18884949d36ff6
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-25 04:15:51 -0500

    Update pedidos_detalle.routes.js

--- src/routes/pedidos_detalle.routes.js
+++ src/routes/pedidos_detalle.routes.js
@@ -5,8 +5,8 @@ import { verificarToken } from "../middlewares/authJWT.js";
 const router=Router()
 
 router.get('/pedidos_detalle',getPedidos_Detalle)  //select
-router.get('/pedidos_detalle/:id',getpedidos_detallexid)  //select x id
 router.get('/pedidos_detalle/resumen', verificarToken, getPedidosResumen);
+router.get('/pedidos_detalle/:id',getpedidos_detallexid)  //select x id
 router.post('/pedidos_detalle',postPedido_Detalle)  //insert
 router.put('/pedidos_detalle/:id',putPedido_Detalle)  //update
 router.patch('/pedidos_detalle/:id',patchPedido_Detalle)  //update
