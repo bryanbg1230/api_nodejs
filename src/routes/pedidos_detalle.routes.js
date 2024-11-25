0834092a6a3559b4d3bb14d3942c9245bbb82bf1
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-25 03:45:48 -0500

    Update pedidos_detalle.routes.js

--- src/routes/pedidos_detalle.routes.js
+++ src/routes/pedidos_detalle.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getPedidos_Detalle, getpedidos_detallexid, postPedido_Detalle, putPedido_Detalle, patchPedido_Detalle, deletePedido_Detalle } from "../controladores/pedidos_detalleCtrl.js"
+import { verificarToken } from "../middlewares/verificarToken.js";
 
 const router=Router()
 
 router.get('/pedidos_detalle',getPedidos_Detalle)  //select
 router.get('/pedidos_detalle/:id',getpedidos_detallexid)  //select x id
+router.get('/pedidos/resumen', verificarToken, getPedidosResumen);
 router.post('/pedidos_detalle',postPedido_Detalle)  //insert
 router.put('/pedidos_detalle/:id',putPedido_Detalle)  //update
 router.patch('/pedidos_detalle/:id',patchPedido_Detalle)  //update
