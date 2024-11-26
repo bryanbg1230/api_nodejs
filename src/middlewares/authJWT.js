bf2dd49eb7fb93be7066debdfe64fecd9800026d
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-26 07:22:29 -0500

    Update authJWT.js

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -7,17 +7,17 @@ export const verificarToken = (req, res, next) => {
     console.log('Authorization header:', token); // Depuración (SA)
     if (!token) return res.status(403).json({ message: 'Token no provisto' });
 
-    if (!token || !token.startsWith('Bearer ')) {    //(SA)
+    /* if (!token || !token.startsWith('Bearer ')) {    //(SA)
         return res.status(401).json({ message: 'Fallo en la autenticación del token' });    //(SA)
-    }    //(SA)
+    }  */   //(SA)
 
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
             console.error('Error al verificar el token:', err); // Agrega esto para depuración
             return res.status(500).json({ message: 'Fallo en la autenticación del token' });
         }
-        req.user = decoded;
-        //req.user = { id: decoded.id }; // Asegúrate de que aquí esté el ID del usuario
+        //req.user = decoded;
+        req.user = { id: decoded.id }; // Asegúrate de que aquí esté el ID del usuario
         next();
     });
 };
