5d29295b2a8b7f73975a983368aa86a6e7f1033b
bryanbg1230 <archivosdepcguardados4@gmail.com>
2024-11-26 07:43:34 -0500

    Update authJWT.js

--- src/middlewares/authJWT.js
+++ src/middlewares/authJWT.js
@@ -3,7 +3,7 @@ import { JWT_SECRET } from '../config.js';
 
 export const verificarToken = (req, res, next) => {
     //const token = req.headers['authorization'];
-    const token = req.headers['authorization']?.split(' ')[1]; // Divide 'Bearer token'
+    const token = req.headers['authorization']?.split(' ')[1]; // Divide 'Bearer token'      //VALE CON ESTO EN LA APP PERO AL PARECER CON EL POSTMAN NO
     console.log('Authorization header:', token); // Depuración (SA)
     if (!token) return res.status(403).json({ message: 'Token no provisto' });
 
@@ -16,8 +16,8 @@ export const verificarToken = (req, res, next) => {
             console.error('Error al verificar el token:', err); // Agrega esto para depuración
             return res.status(500).json({ message: 'Fallo en la autenticación del token' });
         }
-        req.user = decoded;
-        //req.user = { id: decoded.id }; // Asegúrate de que aquí esté el ID del usuario
+        //req.user = decoded;  //VALE CON LA APP PERO CON EL POSTMAN NO
+        req.user = { id: decoded.id }; // Asegúrate de que aquí esté el ID del usuario
         next();
     });
 };
